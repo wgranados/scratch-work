4c892798df3a21065731bcd88fd2263ccc27b474
William G. <will.grand.wg@gmail.com>
2014-09-08 09:15:15 -0400

    Update BacteriaGrowth.cpp

--- BacteriaGrowth.cpp
+++ BacteriaGrowth.cpp
@@ -1,3 +1,12 @@
+/*
+The formula y = ne^kt can be used for estimating growth where:
+y is the final amount n is the initial amount k is a constant t is the time
+For example, this formula could be used for estimating population growth in a region or for estimating cell growth in a lab experiment.
+Create a BacteriaGrowth application that calculates how many bacteria will be present based on this formula.
+The application should prompt the user initial bacteria, the constant k, and the time.
+Refer to Exercise 12 for documentation for the Math methods for this exercise.
+Application output should look similar to:
+*/
 #include<iostream>
 #include<stdio.h>
 #include<cmath>
