bcc44c0317ef898759fbb971f03aff8d135a839e
William G. <will.grand.wg@gmail.com>
2014-09-08 09:16:34 -0400

    Update Decay.cpp

--- Decay.cpp
+++ Decay.cpp
@@ -1,3 +1,16 @@
+/*
+The formula used in Exercise 1 for growth problems can also be used in decay problems. In decay problems, k is negative.
+Create an application that allows the user to select from the following options:
+        •        calculate the final amount: ne–kt
+        •        calculate the initial amount: y / e–kt
+        •        calculate the constant (called the half-life): (log (y/n)) / t 
+(where log e = 0.4343) 
+The application should prompt the user to select one of the three choices and based on the selected option prompts the user to enter the appropriate known information.
+For example, a radioactive mass of 200 grams will reduce to 100 grams in 10 years.
+Based on this information, the half-life is calculated to be –0.06931.
+Refer to Exercise 12 for documentation for the Math methods for this exercise.
+Application output should look similar to:
+*/
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
