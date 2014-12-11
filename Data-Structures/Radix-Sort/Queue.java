3eb0b24f50431037c7489fdc9d0a8110c122e0fe
William G. <will.grand.wg@gmail.com>
2014-12-11 07:55:52 -0500

    Update Queue.java

--- Data-Structures/Radix-Sort/Queue.java
+++ Data-Structures/Radix-Sort/Queue.java
@@ -1,11 +1,17 @@
+/** Name: William Granados
+ *  Date: 06/11/14
+ *  Purpose: Creates a mathematical matrix with varying functions
+ * */
 
 public interface Queue {
-	/**Append object to me*/
-	void enqueue(Object o);
 	
-	/**Return my front element
-	 * Requires.size() != 0*/
+	/**Appends object to the end of a queue data structure*/
+	void enqueue(Object o);
+	/**Pops element at the front of a queue data structure then returns the element*/
+	Object dequeue();
+	/**Returns the element at the front of the queue, element is not popped of queue*/
 	Object head();
-	Object deque();
+	/**Returns the number of elements present in the queues*/
 	int size();
+	
 }
