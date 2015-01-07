c05ae2fa47481782b13049677539010b40cc5171
William G. <will.grand.wg@gmail.com>
2015-01-07 10:56:15 -0500

    Update BST.java

--- Data-Structures/Trees/Binary-Search-Tree/BST.java
+++ Data-Structures/Trees/Binary-Search-Tree/BST.java
@@ -1,57 +1,98 @@
-import java.util.ArrayList;
-import java.util.Queue;
-
-
-public class BST<T extends Comparable<T>> {
+/** Name: William Granados
+ *  Date: 09/11/14
+ *  Purpose: Creates a generic bst
+ * */
+public class Tree<T extends Comparable<T> > {
 	
-	private Node<T>root;
+	public Node<T> root;
+	public int height;
 	
-	public void insert(T value){
-		Node<T>val = new Node<T>(value);
-		Node<T>current = root;
-		// we are initializing the tree
-		if(current == null)
-			this.root = val;
+	public Tree(){}
+	
+	public void insert(Node<T>node){
+		
+		if(this.root == null){
+			node = root;
+			return;
+		}
 		else{
+			Node<T>current = root;
 			// we keep on comparing until we find a value that is null
 			while(true){
 				// less than or equal to current node
-				if(val.getValue().compareTo(current.getValue()) <= 0){
-					if(current.getLeftChild() != null)
-						current = current.getLeftChild();
+				if(node.value.compareTo(current.value) <= 0){
+					if(current.left != null)
+						current = current.left;
 					else{
-						current.setLeftChild(val);
-						val.setParent(current);
+						current.left = node;
 						break;
 					}
 				}
 				// greater than current node
 				else{
-					if(current.getRightChild() != null)
-						current = current.getRightChild();
+					if(current.right != null)
+						current = current.right;
 					else{
-						current.setRightChild(val);
-						val.setParent(current);
+						current.right = node;
+						break;
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public void insert(T value){
+		Node<T>val = new Node<T>(value);
+		// we are initializing the tree
+		if(this.root == null)
+			this.root = val;
+		else{
+			Node<T>current = root;
+			// we keep on comparing until we find a value that is null
+			while(true){
+				// less than or equal to current node
+				if(val.value.compareTo(current.value) <= 0){
+					if(current.left != null)
+						current = current.left;
+					else{
+						current.left = val;
+						break;
+					}
+				}
+				// greater than current node
+				else{
+					if(current.right != null)
+						current = current.right;
+					else{
+						current.right = val;
 						break;
 					}
 				}
 			}
 		}
 	}
-	
-	public String toString(){
-		@SuppressWarnings("unchecked")
-		Queue<Node<T>> q = (Queue<Node<T>>) new ArrayList<Node<T>>();
-		String toadd = "";
-		q.add(this.root);
-		while(!q.isEmpty()){
-			Node<T>current = q.poll();
-			toadd += " " + current.toString();
-			if(current.getLeftChild() != null)
-				q.add(current.getLeftChild());
-			if(current.getRightChild() != null)
-				q.add(current.getRightChild());
-		}
-		return toadd;
+
+	public void preOrder(Node<T>current){
+		System.out.println(current.value);
+		if(current.left != null)
+			preOrder(current.left);
+		if(current.right != null)
+			preOrder(current.right);
 	}
+	public void inOrder(Node<T>current){
+		if(current.left != null)
+			inOrder(current.left);
+		System.out.println(current.value);
+		if(current.right != null)
+			inOrder(current.right);
+	}
+	public void postOrder(Node<T>current){
+		if(current.left != null)
+			postOrder(current.left);
+		if(current.right != null)
+			postOrder(current.right);
+		System.out.println(current.value);
+	}
+	
 }
