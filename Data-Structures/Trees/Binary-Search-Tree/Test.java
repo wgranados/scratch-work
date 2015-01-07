436855f18067423dae66699d91b06fbfa95868f8
William G. <will.grand.wg@gmail.com>
2015-01-07 09:43:19 -0500

    Create Test.java

---
+++ Data-Structures/Trees/Binary-Search-Tree/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	public static void main(String[]args){
+		BST<Integer>bst = new BST<Integer>();
+		bst.insert(1);
+		bst.insert(2);
+		bst.insert(3);
+		System.out.println(bst);
+	}
+}
