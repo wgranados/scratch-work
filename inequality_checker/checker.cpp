65070e36106cad984ebcbb1cb90dde0602530d46
William Granados <will.grand.wg@gmail.com>
2016-04-08 17:48:37 -0400

    checking stuff

---
+++ inequality_checker/checker.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdio>
+using std::cin;
+using std::endl;
+using std::cout;
+
+bool inequality_holds(int prime)
+{
+    // check if the inequality, a^(p-1) = 1 mod(p^2), where a < p
+    int ret = 1;
+    // use modluar arithmetic to avoid integer overflow
+    for(int i = 1;i <= prime-1;i++)
+    {
+        ret *= 2; // let a = 2
+        ret %= prime*prime;
+    }
+    ret %= prime*prime;
+    return (ret == 1);
+}
+
+int main()
+{
+    freopen("primes.txt","r",stdin);
+    int prime = 0;
+    while(cin >> prime)
+    {
+        if(inequality_holds(prime))
+        {
+            cout << prime << endl;
+            break;
+        }
+    }
+    return 0;
+}
